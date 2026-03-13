17e1637125a476821498aebbfc4bb89fb381aca3
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-03-13 22:23:39 +0700

    chore: add helper utilities

---
+++ src/helper/authRefresh.ts
@@ -0,0 +1,38 @@
+import axios from "axios"
+
+const refreshApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true
+})
+
+let refreshPromise: Promise<{ success: boolean }> | null = null
+
+export async function refreshAccessToken() {
+  if (!refreshPromise) {
+    refreshPromise = refreshApi.post('/auth/refresh')
+      .then(response => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch(error => {
+        console.log(error)
+        throw error
+      })
+      .finally(() => {
+
+        /**
+         * Just like the first request can set null to the refreshPromise
+         */
+
+        refreshPromise = null
+      })
+  }
+
+  /**
+   * for the second and subsequent request,
+   * it will wait for the first refresh to complete
+   * and then return the same result of that first refresh
+   */
+
+  return refreshPromise
+}
