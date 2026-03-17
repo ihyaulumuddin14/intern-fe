8b3dc49600fe8c8ed90d63a8246b43635f59335a
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-03-17 11:08:13 +0700

    chore(auth): add setup for auth intercepting route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Lora } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-});
+  weight: ["700", "600", "500", "400"]
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lora = Lora({
+  variable: "--font-lora",
   subsets: ["latin"],
-});
+  style: "italic",
+  weight: ["700", "600", "500", "400"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,15 +22,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${lora.variable} antialiased`}
       >
         {children}
+        {modal}
       </body>
     </html>
   );
