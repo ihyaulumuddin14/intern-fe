8099169e4b09c3ad6d49b8433c5481f78438ac2c
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-03-21 00:08:04 +0700

    fix(onboading): improve performance with deleting realtime persist set

--- src/app/(guest)/onboarding/Components/InputNameStep.tsx
+++ src/app/(guest)/onboarding/Components/InputNameStep.tsx
@@ -2,68 +2,67 @@
 
 import FormStepCard from "@/components/shared/FormStepCard";
 import { Button } from "@/components/ui/button";
-import { Field, FieldGroup } from "@/components/ui/field";
+import { Field, FieldError, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { OnboardingCredentials } from "@/schemas/onboarding.schema";
 import { useOnboardingStep } from "@/stores/useOnboardingStep";
-import { StepDirection } from "@/types/common.type";
 import { ArrowRight } from "lucide-react";
 import { useEffect } from "react";
 import { useFormContext, useWatch } from "react-hook-form";
 
-export default function InputNameStep({
-  direction
-}: {
-  direction: StepDirection
-}) {
+export default function InputNameStep() {
   const {
     register,
     trigger,
     control,
-    formState: { errors }
-  } = useFormContext<OnboardingCredentials>()
-  const { nextStep } = useOnboardingStep()
+    formState: { errors, touchedFields, dirtyFields },
+  } = useFormContext<OnboardingCredentials>();
+  const { nextStep } = useOnboardingStep();
 
   const fullName = useWatch({
     control,
     name: "fullName",
-  })
+  });
 
   useEffect(() => {
-    trigger("fullName");
-  }, [])
-
+    if (dirtyFields.fullName || touchedFields.fullName) {
+      trigger("fullName");
+    }
+  }, [trigger, dirtyFields.fullName, touchedFields.fullName]);
 
   return (
-    <FormStepCard direction={direction} title={
-      <>Mari Mulai dari <span className="text-primary">Namamu</span></>
-    }>
-          <FieldGroup>
-            <Field>
-              <Input
-                className="w-full"
-                id="name-input"
+    <FormStepCard
+      title={
+        <>
+          Mari Mulai dari <span className="text-primary">Namamu</span>
+        </>
+      }
+    >
+      <FieldGroup>
+        <Field>
+          <Input
+            className="w-full"
+            id="name-input"
             placeholder="Masukkan nama lengkap kamu..."
-            value={fullName}
             {...register("fullName")}
-              />
+          />
           {errors.fullName && (
-            <p className="text-base text-right text-error-hover -mt-1">*{errors.fullName.message}</p>
+            <FieldError>{errors.fullName.message}</FieldError>
           )}
-            </Field>
+        </Field>
         <Field>
           <Button
             size="lg"
             className="max-w-fit mx-auto"
             type="button"
-            disabled={!!errors.fullName}
+            disabled={!fullName || !!errors.fullName}
             onClick={nextStep}
           >
-              Lanjut
-              <ArrowRight/>
+            Lanjut
+            <ArrowRight />
           </Button>
         </Field>
-          </FieldGroup>
+      </FieldGroup>
     </FormStepCard>
-  )
+  );
 }
